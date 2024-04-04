60655a563992b378db6db9d6bdd93e7f21a7b740
gideon <gideonmunene0@gmail.com>
2024-04-03 21:11:59 -0400

    map

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (first) => {
+  let cap = tutorials.map((first) => {
+  first.split(" ");
+  console.log(first.split(" "));
+  return first
+  .split(" ")
+  .map((word) => word[0].toUpperCase() + word.substring(1))
+  .join(" ");
+  });
+  return cap;}
+
+  console.log(titleCased);
